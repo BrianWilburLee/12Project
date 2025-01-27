a83be048c07c77697e3f010de311227b95323de6
BrianWilburLee <23016278@myrp.edu.sg>
2025-01-27 15:20:31 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-// App.js (or the entry point of your app)
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
